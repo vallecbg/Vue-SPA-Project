196e74c53bc806efd975a525f93db79cb5dc5c9d
vallecbg <valioplayofficial@gmail.com>
2020-03-13 01:04:51 +0200

    Edited App Navigation

--- cook-book/src/components/AppNavigation.vue
+++ cook-book/src/components/AppNavigation.vue
@@ -1,27 +1,62 @@
 <template>
     <span>
         <v-navigation-drawer app v-model="drawer" class="red lighten-2" disable-resize-watcher dark>
-            
             <v-list>
-                <v-img src="../assets/logo.png" aspect-ratio="1.4"></v-img>
-                <template v-for="item in items">
+                <router-link class="routerLink" to="/">
+                    <v-img src="../assets/logo.png" aspect-ratio="1.4"></v-img>
+                </router-link>
+
+                <v-list-item>
+                    <router-link class="routerLink" to="/about">
+                        <v-list-item-content>
+                            <v-btn text>About</v-btn>
+                        </v-list-item-content>
+                    </router-link>
+                </v-list-item>
+                <v-divider></v-divider>
+                <v-list-item>
+                    <router-link class="routerLink" to="/login">
+                        <v-list-item-content>
+                            <v-btn text>Login</v-btn>
+                        </v-list-item-content>
+                    </router-link>
+                </v-list-item>
+                <v-divider></v-divider>
+                <v-list-item>
+                    <router-link class="routerLink" to="/register">
+                        <v-list-item-content>
+                            <v-btn text>Register</v-btn>
+                        </v-list-item-content>
+                    </router-link>
+                </v-list-item>
+                <v-divider></v-divider>
+                <!-- <template v-for="item in items">
                     <v-list-item :key="item.id">
                         <v-list-item-content>
                             <v-btn text>{{item.title}}</v-btn>
                         </v-list-item-content>
                     </v-list-item>
                     <v-divider :key="`divider-${item.id}`"></v-divider>
-                </template>
+                </template> -->
+
             </v-list>
         </v-navigation-drawer>
         <v-toolbar app color="red lighten-1" dark>
             <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
             <v-spacer class="hidden-md-and-up"></v-spacer>
-            <v-toolbar-title>{{appTitle}}</v-toolbar-title>
-            <v-btn text class="hidden-sm-and-down">Menu</v-btn>
+            <router-link class="routerLink" to="/">
+                <v-toolbar-title to="/">{{appTitle}}</v-toolbar-title>
+            </router-link>
+            <router-link class="routerLink" to="/about">
+                <v-btn text class="hidden-sm-and-down">About</v-btn>
+            </router-link>
             <v-spacer class="hidden-sm-and-down"></v-spacer>
-            <v-btn text class="hidden-sm-and-down">SIGN IN</v-btn>
-            <v-btn text class="hidden-sm-and-down">SIGN UP</v-btn>
+            <router-link class="routerLink" to="/login">
+                <v-btn text class="hidden-sm-and-down">Login</v-btn>
+            </router-link>
+            <router-link class="routerLink" to="/register">
+                <v-btn text class="hidden-sm-and-down">Register</v-btn>
+            </router-link>
         </v-toolbar>
     </span>
 </template>
@@ -31,17 +66,16 @@ export default {
     name: 'AppNavigation',
     data() {
         return {
-            appTitle: "Cook Book",
-            drawer: false,
-            items: [
-                {id: 0, title: 'Menu'},
-                {id: 1, title: 'Sign In'},
-                {id: 2, title: 'Sign Up'}
-            ]
-        }
+            appTitle: 'Cook Book',
+            drawer: false
+        };
     }
 };
 </script>
 
 <style scoped>
+.routerLink {
+    text-decoration: none;
+    color: white;
+}
 </style>
--- cook-book/src/router/index.js
+++ cook-book/src/router/index.js
@@ -1,30 +1,32 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Router from 'vue-router';
 
-Vue.use(VueRouter);
 
-const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home
-    },
-    {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
-];
+Vue.use(Router);
 
-const router = new VueRouter({
+export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
-    routes
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: () => import('../views/Home.vue')
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: () => import('../views/About.vue')
+        },
+        // {
+        //     path: '/register',
+        //     name: 'register',
+        //     component: () => import('./views/Register.vue')
+        // },
+        // {
+        //     path: '/login',
+        //     name: 'login',
+        //     component: () => import('./views/Login.vue')
+        // }
+    ]
 });
-
-export default router;
