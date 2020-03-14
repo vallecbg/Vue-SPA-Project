f7a75c91157045020ed568d43698489244e4c451
vallecbg <valioplayofficial@gmail.com>
2020-03-14 02:39:07 +0200

    Added auth forms

--- cook-book/src/router/index.js
+++ cook-book/src/router/index.js
@@ -18,15 +18,15 @@ export default new Router({
             name: 'about',
             component: () => import('../views/About.vue')
         },
-        // {
-        //     path: '/register',
-        //     name: 'register',
-        //     component: () => import('./views/Register.vue')
-        // },
-        // {
-        //     path: '/login',
-        //     name: 'login',
-        //     component: () => import('./views/Login.vue')
-        // }
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('../views/Login.vue')
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: () => import('../views/Register.vue')
+        }
     ]
 });
---
+++ cook-book/src/views/Login.vue
@@ -0,0 +1,82 @@
+
+<template>
+  <div class="wrraper">
+    <v-row align="center" class="card">
+      <h2>Login</h2>
+      <v-form @submit.prevent="login" v-model="valid" ref="loginForm">
+        <v-text-field v-model="username" :rules="usernameRules" label="Username"></v-text-field>
+        <v-text-field
+          v-model="password"
+          name="password"
+          :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="[passwordRules.required, passwordRules.min]"
+          :type="show ? 'text' : 'password'"
+          label="Password"
+          value
+          class="input-group--focused"
+          @click:append="show = !show"
+        ></v-text-field>
+
+        <v-container class="actions">
+          <v-btn type="submit" :disabled="!valid" color="success" class="mr-4" width="300">Login</v-btn>
+        </v-container>
+        <v-divider></v-divider>
+      </v-form>
+      <p class="text">
+        Don't have an account?
+        <router-link to="/register">Register</router-link>
+      </p>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      valid: true,
+      show: false,
+      checkbox: false,
+      username: "",
+      usernameRules: [
+        v => !!v || "Username is required",
+        v => (v && v.length >= 3) || "Username must be at least 3 characters"
+      ],
+      password: "",
+      passwordRules: {
+        required: v => !!v || "Password is required.",
+        min: v => v.length >= 6 || "Password must be at least 6 characters"
+      }
+    };
+  },
+};
+</script>
+
+<style scoped>
+.actions {
+  margin-bottom: 15px;
+}
+.text {
+  margin-top: 15px;
+}
+.wrraper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 600px;
+  border-radius: 0.5em;
+  box-shadow: 0 0 1em gray;
+  padding: 40px 50px;
+  margin-top: 20px;
+  background: rgba(255, 255, 255, 0.95);
+}
+h2 {
+  margin-bottom: 40px;
+}
+</style>
