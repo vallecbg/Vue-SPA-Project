c6214b23bad73fd54b53828f758b75eee8c403c7
vallecbg <valioplayofficial@gmail.com>
2020-04-08 17:17:30 +0300

    Added transition when route in pages

--- cook-book/src/App.vue
+++ cook-book/src/App.vue
@@ -1,8 +1,10 @@
 <template>
     <v-app>
         <app-navigation></app-navigation>
-        <v-content class="animated fadeIn">
+        <v-content>
+            <transition name="slide-fade" mode="out-in">
             <router-view></router-view>
+            </transition>
         </v-content>
     </v-app>
 </template>
@@ -30,5 +32,17 @@ a {
 .link {
     color: #000 !important;
     text-decoration: none !important;
+}
+
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
 }
 </style>
